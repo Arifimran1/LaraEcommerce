1a39e61a0a41706b239c41d4f4e3f0ef677efcf8
arifimran1 <lime.diamond77@gmail.com>
2019-02-23 22:29:07 +0600

    23 feb commit

--- resources/views/admin/pages/product/index.blade.php
+++ resources/views/admin/pages/product/index.blade.php
@@ -6,9 +6,10 @@
     <div class="content-wrapper">
       <div class="card">
         <div class="card-header">
-          Add Products
+          Products
         </div>
         <div class="card-body">
+          @include('admin.partials.messages')
           <table class="table table-hover table-striped" >
 
             <tr>
@@ -27,28 +28,30 @@
                 <td>
                     <a href="{{ route('admin.product.edit', $product->id) }}" class="btn btn-success">edit</a>
                     <a href="#deleteModal{{$product->id}}" data-toggle="modal" class="btn btn-danger">Delete</a>
-                    
+
                     <!--delete modal-->
                     <div class="modal fade" id="deleteModal{{$product->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog" role="document">
                           <div class="modal-content">
                             <div class="modal-header">
-                              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                              <h5 class="modal-title" id="exampleModalLabel"> Are you sure to delete?</h5>
                               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                               </button>
                             </div>
                             <div class="modal-body">
-                              ...
+                              <form action=" {{route('admin.product.delete', $product->id)}}" method="post">
+                                {{ csrf_field() }}
+                                <button type="submit" class="btn btn-danger">Parmanent delete</button>
+                              </form>
                             </div>
                             <div class="modal-footer">
                               <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                              <button type="button" class="btn btn-primary">Save changes</button>
                             </div>
                           </div>
                         </div>
                       </div>
-                
+
                 </td>
 
               </tr>
--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,18 @@ Route::get('/products', 'PagesController@products')->name('products');
 
 Route::group(['prefix' => 'admin'], function(){
   Route::get('/', 'AdminPagesController@index')->name('admin.pages.index');
-  Route::get('/products', 'AdminPagesController@manage_products')->name('admin.products');
-  Route::get('/product/create', 'AdminPagesController@product_create')->name('admin.product.create');
-  Route::get('/product/edit/{id}', 'AdminPagesController@product_edit')->name('admin.product.edit');
 
-  Route::post('/product/store', 'AdminPagesController@product_store')->name('admin.product.store');
-  Route::post('/product/edit/{id}', 'AdminPagesController@product_update')->name('admin.product.update');
+  Route::group(['prefix' => '/products'], function(){
+    Route::get('/', 'AdminProductController@index')->name('admin.products');
+    Route::get('/create', 'AdminProductController@create')->name('admin.product.create');
+    Route::get('/edit/{id}', 'AdminProductController@edit')->name('admin.product.edit');
+
+    Route::post('/store', 'AdminProductController@store')->name('admin.product.store');
+    Route::post('/product/edit/{id}', 'AdminProductController@update')->name('admin.product.update');
+    Route::post('/product/delete/{id}', 'AdminProductController@delete')->name('admin.product.delete');
+  });
+
+
 
 });
 
